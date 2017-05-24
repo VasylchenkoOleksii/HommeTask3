21819cde7eb2062b0f895bb73b8570a80935ded1
vasylchenko <foxdesert1@rambler.ru>
2017-05-24 15:52:49 +0300

    commit changes

--- src/main/java/HommeTask3/App.java
+++ src/main/java/HommeTask3/App.java
@@ -19,14 +19,10 @@ public class App
         ValidatorFactory vf = Validation.buildDefaultValidatorFactory();
         Validator validator = vf.getValidator();
 
-        Product iphone = new Product("Iphone", new BigDecimal(0.01),new Date(11),new Date(40));
+        Product iphone = new Product("Iphone", new BigDecimal(0.01),new Date(51),new Date(40));
+        Order order = new Order(iphone, 6, "Самый смешной телефон");
 
-
-        Order order = new Order(iphone, 5, "Самый смешной телефон");
-        Set<ConstraintViolation<Order>> violations = validator.validate(order,MyGroup.class);
-        System.out.println(violations);
-
-        Set<ConstraintViolation<Order>> validate = validator.validate(order);
+        Set<ConstraintViolation<Order>> validate = validator.validate(order,MyGroup.class/*,MyGroup2.class,Default.class*/);
 
         System.out.println(":::: validate = " + validate.size());
         for (ConstraintViolation<Order> s : validate) {
